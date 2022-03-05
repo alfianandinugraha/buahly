99adb42deef92001a72490af132afd5148d9b9d7
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-06 01:10:01 +0700

    feat(search_field.dart): create design for focus TextField

--- lib/views/widgets/search_field.dart
+++ lib/views/widgets/search_field.dart
@@ -2,46 +2,64 @@ import 'package:buahly/themes/colors/primary_colors.dart';
 import 'package:buahly/themes/icons/asset_icon.dart';
 import 'package:flutter/material.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
   const SearchField({Key? key}) : super(key: key);
 
   @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 14),
-            child: Icon(
-              AssetIcon.search,
-              size: 16,
-              color: PrimaryColors.pallete[500]!,
-            )
-          )
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 11),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4),
-            border: Border.all(
-              color: const Color(0xFFE3E3E3)
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(focusNode);
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 14),
+              child: Icon(
+                AssetIcon.search,
+                size: 16,
+                color: PrimaryColors.pallete[500]!,
+              )
             )
           ),
-          child: const TextField(
-            decoration: InputDecoration.collapsed(
-              hintText: "Search Fruit",
-              hintStyle: TextStyle(
-                color: Color(0xFFE3E3E3)
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 11),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(
+                color: focusNode.hasFocus ? PrimaryColors.pallete[500]! : const Color(0xFFE3E3E3)
               )
             ),
-            style: TextStyle(
-              fontSize: 14
+            child: TextField(
+              focusNode: focusNode,
+              decoration: const InputDecoration.collapsed(
+                hintText: "Search Fruit",
+                hintStyle: TextStyle(
+                  color: Color(0xFFE3E3E3)
+                )
+              ),
+              style: const TextStyle(
+                fontSize: 14
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
