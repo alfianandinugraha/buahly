d7dca2950a2e34ec13f575f54ff7115f66f31add
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-05 22:47:33 +0700

    feat(assets): add font icons

---
+++ lib/themes/icons/asset_icon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AssetIcon {
+  AssetIcon._();
+
+  static const _kFontFam = 'Icons';
+  static const String? _kFontPkg = null;
+
+  static const IconData arrowLeft = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData duoChevron = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData info = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
