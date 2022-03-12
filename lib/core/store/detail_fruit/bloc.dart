80b83050a8d2cab530772de9cc2e7e9032a26aa2
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-13 00:49:06 +0700

    feat(detail_fruit/bloc.dart): add cache for 5 seconds

--- lib/core/store/detail_fruit/bloc.dart
+++ lib/core/store/detail_fruit/bloc.dart
@@ -1,21 +1,48 @@
+import 'dart:convert';
+
 import 'package:buahly/core/models/fruit.dart';
 import 'package:buahly/core/repositories/fruit_repository.dart';
 import 'package:buahly/core/store/detail_fruit/event.dart';
 import 'package:buahly/core/store/detail_fruit/state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 
 class DetailFruitBloc extends Bloc<DetailFruitEvent, DetailFruitState> {
   DetailFruitBloc() : super(DetailFruitEmpty()) {
     on<DetailFruitEvent>((event, emit) async {
+      var box = await Hive.openBox("fruit");
+
       if (event is FetchDetailFruit) {
+        var cachedFruitJSON = box.get(event.id);
+
+        if (cachedFruitJSON != null) {
+          var cachedFruit = json.decode(cachedFruitJSON);
+          var duration = 5000;
+
+          if (cachedFruit['time'] + duration > DateTime.now().millisecondsSinceEpoch) {
+            var fruit = Fruit.fromMap(cachedFruit['body']);
+            emit(DetailFruitLoaded(fruit: fruit));
+            return;
+          }
+        }
+
         emit(DetailFruitFetched());
 
         try {
           var response = await FruitRepository().getById(event.id);
           var fruit = Fruit.fromJson(response.body);
           emit(DetailFruitLoaded(fruit: fruit));
+
+          DateTime time = DateTime.now();
+          Map<String, dynamic> cache = {
+            'time': time.millisecondsSinceEpoch,
+            'body': fruit.toMap()
+          };
+          box.put(event.id, json.encode(cache));
         } catch (err) {
           emit(DetailFruitError());
+        } finally {
+          // await box.close();
         }
       }
     });
