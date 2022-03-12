e5666c3941120ff5b6917e4f130fca7bf34ace16
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-12 15:50:55 +0700

    feat(widgets): create NutritionProperty widget

--- lib/views/pages/detail_page.dart
+++ lib/views/pages/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:buahly/themes/colors/primary_colors.dart';
 import 'package:buahly/themes/icons/asset_icon.dart';
 import 'package:buahly/views/layouts/base_layout.dart';
+import 'package:buahly/views/widgets/nutrition_property.dart';
 import 'package:buahly/views/widgets/property.dart';
 import 'package:flutter/material.dart';
 
@@ -75,6 +76,57 @@ class DetailPage extends StatelessWidget {
                 child: Text("Zingiberales")
               )
             ],
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: const EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              color: PrimaryColors.pallete[100]?.withOpacity(0.075),
+              borderRadius: BorderRadius.circular(6),
+              border: Border.all(
+                color: PrimaryColors.pallete[500]!,
+                width: 1
+              )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Nutritions",
+                  style: TextStyle(
+                    color: PrimaryColors.pallete[500],
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    NutritionProperty(label: "Sugar", total: 30),
+                    NutritionProperty(label: "Protein", total: 1),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    NutritionProperty(label: "Fat", total: 0.2),
+                    NutritionProperty(label: "Calories", total: 96),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    NutritionProperty(label: "carbohydrates", total: 22),
+                  ],
+                ),
+              ],
+            )
           )
         ],
       )
---
+++ lib/views/widgets/nutrition_property.dart
@@ -0,0 +1,43 @@
+import 'package:buahly/themes/colors/primary_colors.dart';
+import 'package:buahly/views/widgets/property.dart';
+import 'package:flutter/material.dart';
+
+class NutritionProperty extends StatelessWidget {
+  final String label;
+  final double total;
+
+  const NutritionProperty({
+    Key? key,
+    required this.label,
+    required this.total
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Property(
+      label: label,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            total.toString(),
+            style: TextStyle(
+              fontSize: Theme.of(context).textTheme.headline1?.fontSize,
+              fontWeight: Theme.of(context).textTheme.headline1?.fontWeight,
+              color: PrimaryColors.pallete[500]
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 2, bottom: 2),
+            child: Text(
+              "mg",
+              style: TextStyle(
+                fontSize: Theme.of(context).textTheme.caption?.fontSize,
+              ),
+            ),
+          )
+        ],
+      ) 
+    );
+  } 
+}
