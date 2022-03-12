6989c273de7db5bcf0edd9329978bf82523692cb
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-12 20:35:53 +0700

    feat(pages/detail_page.dart): integrate data from bloc

--- lib/views/pages/detail_page.dart
+++ lib/views/pages/detail_page.dart
@@ -1,9 +1,13 @@
+import 'package:buahly/core/store/detail_fruit/bloc.dart';
+import 'package:buahly/core/store/detail_fruit/event.dart';
+import 'package:buahly/core/store/detail_fruit/state.dart';
 import 'package:buahly/themes/colors/primary_colors.dart';
 import 'package:buahly/themes/icons/asset_icon.dart';
 import 'package:buahly/views/layouts/base_layout.dart';
 import 'package:buahly/views/templates/detail/fruit_info.dart';
 import 'package:buahly/views/templates/detail/nutrition_info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DetailPageArguments {
   final int id;
@@ -40,18 +44,55 @@ class DetailPage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 18),
-          Text(
-            "Banana",
-            style: TextStyle(
-              fontSize: Theme.of(context).textTheme.headline1?.fontSize,
-              fontWeight: Theme.of(context).textTheme.headline1?.fontWeight,
-              color: PrimaryColors.pallete[500]
-            )
+          
+          BlocBuilder<DetailFruitBloc, DetailFruitState>(
+            builder: (_, state) {
+              if (state is DetailFruitEmpty) {
+                BlocProvider
+                  .of<DetailFruitBloc>(context)
+                  .add(FetchDetailFruit(id: id));
+              }
+
+              if (state is DetailFruitFetched) {
+                return Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      Text("Getting fruit...")
+                    ],
+                  ),
+                );
+              }
+
+              if (state is DetailFruitLoaded) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      state.fruit.name,
+                      style: TextStyle(
+                        fontSize: Theme.of(context).textTheme.headline1?.fontSize,
+                        fontWeight: Theme.of(context).textTheme.headline1?.fontWeight,
+                        color: PrimaryColors.pallete[500]
+                      )
+                    ),
+                    const SizedBox(height: 20),
+                    const FruitInfo(),
+                    const SizedBox(height: 20),
+                    const NutritionInfo()
+                  ],
+                );
+              }
+
+              return Container();
+            }
           ),
-          const SizedBox(height: 20),
-          const FruitInfo(),
-          const SizedBox(height: 20),
-          const NutritionInfo()
         ],
       )
     );
--- lib/views/templates/detail/fruit_info.dart
+++ lib/views/templates/detail/fruit_info.dart
@@ -1,41 +1,52 @@
+import 'package:buahly/core/store/detail_fruit/bloc.dart';
+import 'package:buahly/core/store/detail_fruit/state.dart';
 import 'package:buahly/views/widgets/property.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FruitInfo extends StatelessWidget {
   const FruitInfo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: const [
-            Property(
-              label: "id",
-              child: Text("6")
-            ),
-            Property(
-              label: "genus",
-              child: Text("Musa")
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 18,
-        ),
-        Row(
-          children: const [
-            Property(
-              label: "family",
-              child: Text("Musaceae")
-            ),
-            Property(
-              label: "order",
-              child: Text("Zingiberales")
-            )
-          ],
-        )
-      ],
+    return BlocBuilder<DetailFruitBloc, DetailFruitState>(
+      builder: (_, state) {
+        if (state is DetailFruitLoaded) {
+          final fruit = state.fruit;
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Property(
+                    label: "id",
+                    child: Text(fruit.id.toString())
+                  ),
+                  Property(
+                    label: "genus",
+                    child: Text(fruit.genus)
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              Row(
+                children: [
+                  Property(
+                    label: "family",
+                    child: Text(fruit.family)
+                  ),
+                  Property(
+                    label: "order",
+                    child: Text(fruit.order)
+                  )
+                ],
+              )
+            ],
+          );
+        }
+        return Container();
+      }
     );
   }
 }
