5031d499da4157ad1d4c91d7e902bf3ec67b87ff
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-13 14:50:05 +0700

    feat(app): change app name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Buahly',
       theme: LightMode.theme(),
       initialRoute: "/",
       onGenerateRoute: _router.generate,
