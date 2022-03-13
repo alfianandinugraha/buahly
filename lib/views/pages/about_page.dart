590f5b6659d4d9e0883cdf6aca8f538b2923fdd1
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-13 13:48:35 +0700

    feat(pages/about_page.dart): add link on button and update text

--- lib/views/pages/about_page.dart
+++ lib/views/pages/about_page.dart
@@ -2,13 +2,19 @@ import 'package:buahly/themes/base/text_base.dart';
 import 'package:buahly/themes/colors/primary_colors.dart';
 import 'package:buahly/themes/icons/asset_icon.dart';
 import 'package:buahly/views/layouts/base_layout.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   static void go(BuildContext context) {
     Navigator.pushNamed(context, '/about');
+  }
+
+  void openLink(String url) async {
+    if (!await launch(url)) print("Failed to open source code");
   }
 
   @override
@@ -52,17 +58,32 @@ class AboutPage extends StatelessWidget {
                   text: "https://www.fruityvice.com/",
                   style: TextStyle(
                     color: PrimaryColors.pallete[500]
-                  )
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = () {
+                    openLink("https://www.fruityvice.com/");
+                  }
                 ),
                 const TextSpan(
-                  text: " (Thank you Fruityvice 🎉)."
+                  text: " using "
+                ),
+                TextSpan(
+                  text: "https://allorigins.win",
+                  style: TextStyle(
+                    color: PrimaryColors.pallete[500]
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap = () {
+                    openLink("https://allorigins.win");
+                  }
+                ),
+                const TextSpan(
+                  text: " (Thank you Fruityvice and allOrigins 🎉)."
                 )
               ]
             ),
           ),
           const SizedBox(height: 28),
           ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: () => openLink("https://github.com/alfianandinugraha/buahly"),
             label: const Text("Source code"),
             icon: const Icon(
               AssetIcon.github,
@@ -71,7 +92,7 @@ class AboutPage extends StatelessWidget {
           ),
           const SizedBox(height: 12),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () => openLink("https://alfianandi.dev"),
             label: const Text("About me"),
             icon: const Icon(
               AssetIcon.user,
